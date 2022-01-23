ad6cf3678e093aedb82a77e29aa44716ff7781a0
kyledvs <kyledvs58@gmail.com>
2022-01-23 22:12:19 +0200

    after firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //a{ path: 'landing', component: FirstComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dean-and-kyle',
+    appId: '1:637066390699:web:04de8bad76bdb0e0dc1bee',
+    databaseURL: 'https://dean-and-kyle-default-rtdb.firebaseio.com',
+    storageBucket: 'dean-and-kyle.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyADTNtxk4kAwEKo5GuxUzXEa2bobdbfhbs',
+    authDomain: 'dean-and-kyle.firebaseapp.com',
+    messagingSenderId: '637066390699',
+    measurementId: 'G-K6B309VVFR',
+  },
   production: false
 };
 
